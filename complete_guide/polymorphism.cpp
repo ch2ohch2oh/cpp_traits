8872e5db1805d7d3b565d9b9c59b82f0ea0f236a
Dazhi Wang <ch2ohch2oh@gmail.com>
2024-12-20 20:15:59 +0000

    Add ch18 the polymorphic power of templates

---
+++ complete_guide/polymorphism.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+// Dynamic vs static polymorphism
+
+// Concepts
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "done" << std::endl;
+    return 0;
+}
